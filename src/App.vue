d608e94fd6b71228156920ce82e73bda16fcfbca
Luis Victorero <lvictorerod@gmail.com>
2020-12-30 22:04:22 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Personas</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <formulario-persona @add-persona="agregarPersona" />
+        <!--también puedes usar v-bind: en lugar de :, que es la forma larga de agregar propiedades:-->
+        <tabla-personas
+          :personas="personas"
+          @delete-persona="eliminarPersona"
+          @actualizar-persona="actualizarPersona"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaPersonas from "./components/TablaPersonas.vue";
+import FormularioPersona from "./components/FormularioPersona.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TablaPersonas,
+    FormularioPersona,
+  },
+  data() {
+    return {
+      personas: [
+        {
+          id: 1,
+          nombre: "Jon",
+          apellido: "Nieve",
+          email: "jon@email.com",
+        },
+        {
+          id: 2,
+          nombre: "Tyrion",
+          apellido: "Lannister",
+          email: "tyrion@email.com",
+        },
+        {
+          id: 3,
+          nombre: "Daenerys",
+          apellido: "Targaryen",
+          email: "daenerys@email.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    agregarPersona(persona) {
+      let id = 0;
+
+      if (this.personas.length > 0) {
+        id = this.personas[this.personas.length - 1].id + 1;
+      }
+
+      /* Se utiliza el operador spread de propagación ..., útil para combinar objetos y arrays, 
+      para crear un nuevo array que contenga los elemento antiguos del array personas junto con la nueva. */
+      this.personas = [...this.personas, { ...persona, id }];
+    },
+    eliminarPersona(id) {
+      this.personas = this.personas.filter((persona) => persona.id !== id);
+    },
+    actualizarPersona(id, personaActualizada) {
+      this.personas = this.personas.map((persona) =>
+        persona.id === id ? personaActualizada : persona
+      );
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}*/
+
+body {
+  background: #f9f9f9;
 }
 </style>
